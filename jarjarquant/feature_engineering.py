2d1ca451320919b93d3b12f455f76b968506510f
viksharma04 <me@vik-sharma.com>
2024-10-03 23:49:31 -0400

    create a feature engineering module

--- jarjarquant/feature_engineering.py
+++ jarjarquant/feature_engineering.py
@@ -12,7 +12,8 @@ class FeatureEngineering:
         """
         self.features_df = features_df
 
-    def getWeights(d, size):
+    @staticmethod
+    def get_weights(d, size):
 
         w=[1.]
         for k in range(1,size):
@@ -20,3 +21,20 @@ class FeatureEngineering:
             w.append(w_)
         w=np.array(w[::-1]).reshape(-1,1)
         return w
+    
+    def frac_diff(self, d, thres=.01):
+
+        w=self.get_weights(d, self.features_df.shape[0])
+        w_=np.cumsum(abs(w))
+        w_/=w_[-1]
+        skip = w_[w_>thres].shape[0]
+        df={}
+        for name in self.features_df.columns:
+            seriesF,df_=self.features_df[[name]].fillna(method='ffill').dropna(),pd.Series(index=self.features_df.index, dtype=float)
+            for iloc in range(skip,seriesF.shape[0]):
+                loc=seriesF.index[iloc]
+                if not np.isfinite(self.features_df.loc[loc,name]): continue
+                df_[loc]=np.dot(w[-(iloc+1):,:].T,seriesF.loc[:loc])[0,0]
+            df[name]=df_.copy(deep=True)
+        df=pd.concat(df,axis=1)
+        return df

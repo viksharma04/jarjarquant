e763290cdd7ea0194e6c5176030ff35e6b72b4d5
viksharma04 <me@vik-sharma.com>
2024-10-14 19:33:17 -0400

    update setup and init

--- jarjarquant/__init__.py
+++ jarjarquant/__init__.py
@@ -1,7 +1,9 @@
 "Initialize jarjarquant"
 try:
     from .feature_engineering import FeatureEngineering
+    from .labelling import Labelling
 except ImportError as e:
     from feature_engineering import FeatureEngineering
+    from labelling import Labelling
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
+
 setup(
     name='jarjarquant',
-    version='1.0.0',
+    version='0.1.0',
+    description='Implements common financial ML techniques',
+    author='Vik Sharma',
+    url='https://github.com/viksharma04/jarjarquant.git',
     packages=find_packages(),
     install_requires=[]
 )

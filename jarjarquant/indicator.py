a3638d45847bad4e0e09aac1aa7e1135b835bd75
viksharma04 <me@vik-sharma.com>
2025-01-03 17:20:51 -0500

    Add Moving Average Difference and Price Intensity indicators to Jarjarquant class

--- jarjarquant/indicator.py
+++ jarjarquant/indicator.py
@@ -213,6 +213,97 @@ class StochasticRSI(Indicator):
         return sto_rsi
 
 
+class MovingAverageDifference(Indicator):
+    """
+    A class to calculate the Moving Average Difference (MAD) indicator.
+    The MAD indicator is a normalized difference between a short-term and a long-term moving average,
+    adjusted by the Average True Range (ATR) to account for volatility.
+    Attributes:
+        ohlcv_df (pd.DataFrame): DataFrame containing OHLCV (Open, High, Low, Close, Volume) data.
+        short_period (int): The period for the short-term moving average. Default is 5.
+        long_period (int): The period for the long-term moving average. Default is 20.
+        indicator_type (str): Type of the indicator, set to 'continuous'.
+    Methods:
+        calculate() -> np.ndarray:
+            Calculates the Moving Average Difference (MAD) indicator.
+            Returns:
+                np.ndarray: The calculated MAD values.
+    """
+
+    def __init__(self, ohlcv_df: pd.DataFrame, short_period: int = 5, long_period: int = 20):
+        super().__init__(ohlcv_df)
+        self.short_period = short_period
+        self.long_period = long_period
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        # Calculate the short-term MA
+        short_ma = pd.Series(close).rolling(
+            window=self.short_period).mean().values
+
+        # Calculate the long-term MA
+        long_ma = pd.Series(close).rolling(window=self.long_period).mean()
+        # Lag long_ma by short_period
+        long_ma = long_ma.shift(self.short_period).values
+
+        # See pg 116 eq. 4.7 and 4.8 of Statistically Sound Indicators
+        atr_values = self.data_analyst.atr(
+            self.short_period+self.long_period, self.df['Open'], self.df['High'], self.df['Low'], self.df['Close']).values
+
+        denom = atr_values * \
+            np.sqrt((0.5*(self.long_period-1)+self.short_period) -
+                    (0.5*(self.short_period-1)))
+
+        norm_diff = (short_ma - long_ma) / denom
+        COMPRESSION_FACTOR = 1.5
+        mad = 100 * norm.cdf(COMPRESSION_FACTOR*norm_diff) - 50
+
+        # Replace nan values with 0
+        mad = np.where(np.isnan(mad), 0, mad)
+
+        return mad
+
+
+class MACD(Indicator):
+
+    def __init__(self, ohlcv_df: pd.DataFrame, short_period: int = 5, long_period: int = 20, smoothing_factor: int = 2, return_raw_macd: bool = False):
+        super().__init__(ohlcv_df)
+        self.short_period = short_period
+        self.long_period = long_period
+        self.smoothing_factor = smoothing_factor
+        self.indicator_type = 'continuous'
+        self.return_raw_macd = return_raw_macd
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        short_ema = pd.Series(close).ewm(
+            span=self.short_period, adjust=False).mean().values
+        long_ema = pd.Series(close).ewm(
+            span=self.long_period, adjust=False).mean().values
+
+        atr_values = self.data_analyst.atr(
+            self.short_period+self.long_period, self.df['Open'], self.df['High'], self.df['Low'], self.df['Close']).values
+
+        denom = atr_values * \
+            np.sqrt((0.5*(self.long_period-1)+self.short_period) -
+                    (0.5*(self.short_period-1)))
+
+        norm_diff = (short_ema - long_ema) / denom
+        COMPRESSION_FACTOR = 1.0
+        macd = 100 * norm.cdf(COMPRESSION_FACTOR*norm_diff) - 50
+
+        # Replace nan values with 0
+        macd = np.where(np.isnan(macd), 0, macd)
+
+        if self.return_raw_macd:
+            return macd
+        else:
+            signal_line = pd.Series(macd).ewm(
+                span=self.smoothing_factor, adjust=False).mean().values
+            return macd - signal_line
+
+
 class CMMA(Indicator):
     """Cumulative Moving Mean Average (CMMA) Indicator
     This class calculates the CMMA indicator, which is a normalized measure of the 
@@ -276,4 +367,96 @@ class CMMA(Indicator):
         output = 100 * norm.cdf(normalized_output) - 50
 
         # Return the final CMMA values as a pandas Series with the same index as the input DataFrame
+        return output
+
+
+class RegressionTrend(Indicator):
+
+    def __init__(self, ohlcv_df: pd.DataFrame, lookback: int = 21, atr_length_mult: int = 3, degree: int = 1):
+        super().__init__(ohlcv_df)
+        self.lookback = lookback
+        self.degree = degree
+        self.atr_length = atr_length_mult * lookback
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        n = len(close)
+        output = np.full(n, 0.0)
+
+        if self.lookback > n:
+            raise ValueError(
+                "Lookback period is greater than the number of data points!")
+
+        # Calculate the Legendre polynomials
+        lgdre = self.data_analyst.compute_legendre_coefficients(
+            self.lookback, self.degree)
+        atr = self.data_analyst.atr(
+            self.atr_length, self.df['Open'], self.df['High'], self.df['Low'], self.df['Close']).values
+        COMPRESSION_FACTOR = 1.5
+
+        for i in range(self.atr_length+1, n):
+            prices = np.log(close[i-self.lookback:i])
+
+            # Calculate the regression coefficient
+            reg_coeff = self.data_analyst.calculate_regression_coefficient(
+                prices, lgdre)
+
+            # Contextualize the regression coefficient using long term atr
+            reg_coeff = (2*reg_coeff)/(atr[i]*(self.lookback-1))
+
+            # Calculate the R^2 value over the lookback period
+            y_diff = prices - np.mean(prices)
+            y_diff_sq = np.sum(y_diff**2)
+            predictions = reg_coeff * lgdre
+            residuals = y_diff - predictions
+            residuals_sq = np.sum(residuals**2)
+
+            r_squared = 1 - residuals_sq/y_diff_sq
+
+            # Multiply the regression coefficient by the R^2 value to devalue weak trends
+            norm_coeff = reg_coeff * r_squared
+            output[i] = 100 * norm.cdf(COMPRESSION_FACTOR*norm_coeff) - 50
+
+        # Replace nan and inf values with 0
+        output = np.where(np.isnan(output), 0, output)
+
+        return output
+
+
+class PriceIntensity(Indicator):
+
+    def __init__(self, ohlcv_df: pd.DataFrame, smoothing_factor: int = 2):
+        super().__init__(ohlcv_df)
+        self.smoothing_factor = smoothing_factor
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        high = self.df['High'].values
+        low = self.df['Low'].values
+        _open = self.df['Open'].values
+
+        n = len(close)
+        output = np.full(n, 0.0)
+
+        # Special case for the first value
+        output[0] = (close[0] - _open[0]) / (high[0] - low[0])
+
+        # Calculate Raw Price Intensity
+        for i in range(1, n):
+            denom = np.max(high[i]-low[i], high[i] -
+                           close[i-1], close[i-1]-low[i])
+            output[i] = (close[i] - _open[i]) / denom
+
+        # Smooth the Price Intensity values
+        output = pd.Series(output).ewm(
+            span=self.smoothing_factor).mean().values
+
+        # Normalize the Price Intensity values
+        output = 100 * norm.cdf(0.8*np.sqrt(self.smoothing_factor)*output) - 50
+
+        # Replace nan and inf values with 0
+        output = np.where(np.isnan(output), 0, output)
+
         return output

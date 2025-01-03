f0acde9d0a0cfa81e4bebace9baa2912c3bfb63f
viksharma04 <me@vik-sharma.com>
2025-01-02 20:37:20 -0500

    Enhance Indicator class with new methods and improved evaluation reporting; add DetrendedRSI, Stochastic, and StochasticRSI classes

--- jarjarquant/indicator.py
+++ jarjarquant/indicator.py
@@ -1,10 +1,12 @@
 """Indicator class - each indicator in jarjarquant is an instance of the Indicator class.
 Each indicator is a specific calculation on market data with an underlying market hypothesis"""
 
+from typing import Optional
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from .data_analyst import DataAnalyst
+from .feature_engineer import FeatureEngineer
 
 
 class Indicator:
@@ -17,6 +19,7 @@ class Indicator:
         self.df = ohlcv_df.copy()
         self.indicator_type = None
         self.data_analyst = DataAnalyst()
+        self.feature_engineer = FeatureEngineer()
 
     def calculate(self):
         """Placeholder - to be implemented in derived classes
@@ -27,17 +30,22 @@ class Indicator:
         raise NotImplementedError(
             "Derived classes must implement the calculate method.")
 
-    def indicator_evaluation_report(self, n_bins_to_discretize: int = 10):
+    def indicator_evaluation_report(self, transform: Optional[str] = None, n_bins_to_discretize: Optional[int] = 10, **kwargs):
         """Runs a set of statistical tests to examine various properties of the 
-        indicator series, such as, stationarity, normality, entropy, mutual
+        indicator series, such as stationarity, normality, entropy, mutual
         information, etc.
 
         Args:
-            n_bins_to_discretize (int, optional): number of bins to use if indicator
-            is continous. Used for mutual information calculation. Defaults to 10.
+            transform (str, optional): Acceptable values: 'log', 'root', 'tanh'. Transformation to apply to the indicator values.
+            n_bins_to_discretize (int, optional): Number of bins to use if indicator
+            is continuous. Used for mutual information calculation. Defaults to 10.
         """
         values = self.calculate()
-        if self.indicator_type == 'c':
+        if transform is not None:
+            values = self.feature_engineer.transform(
+                values, transform, **kwargs)
+
+        if self.indicator_type == 'continuous':
             d_values = self.data_analyst.discretize_array(
                 values, n_bins_to_discretize)
         else:
@@ -63,7 +71,7 @@ class RSI(Indicator):
     def __init__(self, ohlcv_df: pd.DataFrame, period: int = 14):
         super().__init__(ohlcv_df)
         self.period = period
-        self.indicator_type = 'c'  # 'c' for continuous, 'd' for discrete
+        self.indicator_type = 'continuous'  # continuous or discrete
 
     def calculate(self) -> np.ndarray:
         close = self.df['Close'].values
@@ -94,18 +102,138 @@ class RSI(Indicator):
                 upsum *= (self.period - 1) / self.period
 
             # RSI calculation
-            output[i] = 100.0 * upsum / (upsum + dnsum)
+            if upsum + dnsum == 0:
+                output[i] = 50.0  # Default RSI value when both sums are zero
+            else:
+                output[i] = 100.0 * upsum / (upsum + dnsum)
 
         return output
 
 
+class DetrendedRSI(Indicator):
+    """Class to calculate the Detrended RSI"""
+
+    def __init__(self, ohlcv_df: pd.DataFrame, short_period: int = 2, long_period: int = 21, regression_length: int = 120):
+        super().__init__(ohlcv_df)
+        self.short_period = short_period
+        self.long_period = long_period
+        self.regression_length = regression_length
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        n = len(close)
+        output = np.full(n, 0.0)
+
+        short_rsi = RSI(self.df, self.short_period).calculate()
+        # Apply inverse logistic transformation to the short RSI values
+        # Look at pg 103 of Statistically Sound Indicators
+        short_rsi = -10*np.log((2/(1+0.00999*(2*short_rsi-100))) - 1)
+
+        # Long RSI
+        long_rsi = RSI(self.df, self.long_period).calculate()
+
+        for i in range(self.regression_length+self.long_period-1, n):
+            x = long_rsi[i-self.regression_length:i]
+            y = short_rsi[i-self.regression_length:i]
+
+            x_mean = np.mean(x)
+            y_mean = np.mean(y)
+
+            x_diff = x - x_mean
+            y_diff = y - y_mean
+
+            coef = np.dot(x_diff, y_diff) / (np.dot(x_diff, x_diff) + 1e-10)
+
+            output[i] = (y[-1] - y_mean) - coef * (x[-1] - x_mean)
+
+        return output
+
+
+class Stochastic(Indicator):
+    """Class to calculate the stochastic oscillator"""
+
+    def __init__(self, ohlcv_df, lookback: int = 14, n_smooth: int = 2):
+        super().__init__(ohlcv_df)
+        self.lookback = lookback
+        self.n_smooth = n_smooth
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        close = self.df['Close'].values
+        n = len(close)
+        output = np.full(n, 50.0)
+
+        # Calculate rolling max and min for Close values
+        high_max = pd.Series(close).rolling(window=self.lookback).max().values
+        low_min = pd.Series(close).rolling(window=self.lookback).min().values
+
+        for i in range(self.lookback, n):
+            if high_max[i] == low_min[i]:
+                output[i] = 50.0
+            else:
+                sto_0 = 100 * (close[i] - low_min[i]) / \
+                    (high_max[i] - low_min[i])
+                if self.n_smooth == 0:
+                    output[i] = sto_0
+                elif self.n_smooth == 1:
+                    if i == self.lookback:
+                        output[i] = sto_0
+                    else:
+                        output[i] = 0.33333 * sto_0 + 0.66667 * output[i - 1]
+                else:
+                    if i < self.lookback + 1:
+                        output[i] = sto_0
+                    elif i == self.lookback + 1:
+                        output[i] = 0.33333 * sto_0 + 0.66667 * output[i - 1]
+                    else:
+                        sto_1 = 0.33333 * sto_0 + 0.66667 * output[i - 1]
+                        output[i] = 0.33333 * sto_1 + 0.66667 * output[i - 2]
+
+        return output
+
+
+class StochasticRSI(Indicator):
+    """Class to calculate the Stochastic RSI indicator"""
+
+    def __init__(self, ohlcv_df: pd.DataFrame, rsi_period: int = 14, stochastic_period: int = 14, n_smooth: int = 2):
+        super().__init__(ohlcv_df)
+        self.rsi_period = rsi_period
+        self.stochastic_period = stochastic_period
+        self.n_smooth = n_smooth
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+        rsi = RSI(self.df, self.rsi_period).calculate()
+        # Store RSI values in a DataFrame and rename the column to 'Close'
+        rsi_df = pd.DataFrame(rsi, columns=['Close'])
+        sto_rsi = Stochastic(rsi_df, self.stochastic_period,
+                             self.n_smooth).calculate()
+
+        return sto_rsi
+
+
 class CMMA(Indicator):
+    """Cumulative Moving Mean Average (CMMA) Indicator
+    This class calculates the CMMA indicator, which is a normalized measure of the 
+    logarithmic difference between the closing prices and their rolling mean, adjusted 
+    by the Average True Range (ATR).
+    Attributes:
+        ohlcv_df (pd.DataFrame): A pandas DataFrame containing 'Close', 'Open', 'Low', 
+                                 and 'High' columns.
+        lookback (int): The number of periods for calculating the rolling mean. Default is 21.
+        atr_length (int): The length of the rolling window for the ATR calculation. Default is 21.
+        indicator_type (str): The type of the indicator, set to 'continuous'.
+    Methods:
+        calculate() -> np.ndarray:
+            Calculate the CMMA values.
+                np.ndarray: A numpy array with the CMMA values."""
 
     def __init__(self, ohlcv_df: pd.DataFrame, lookback: int = 21, atr_length: int = 21):
         super().__init__(ohlcv_df)
         self.lookback = lookback
         self.atr_length = atr_length
-        self.indicator_type = 'c'  # 'c' for continuous, 'd' for discrete
+        self.indicator_type = 'continuous'
 
     def calculate(self) -> np.ndarray:
         """
@@ -130,8 +258,7 @@ class CMMA(Indicator):
         log_close = np.log(Close)
 
         # Calculate the rolling mean of the log of close prices over the lookback period
-        rolling_mean = log_close.shift(1).rolling(
-            window=self.lookback).ewm.mean()
+        rolling_mean = log_close.shift(1).ewm(span=self.lookback).mean()
 
         # Calculate the denominator using the ATR function and adjust by the sqrt of (lookback + 1)
         denom = self.data_analyst.atr(self.atr_length, Open, High, Low, Close) * \

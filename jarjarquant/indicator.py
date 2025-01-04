354e5f5e5b3c1d160a5eb26627ba0081abc2c103
viksharma04 <me@vik-sharma.com>
2025-01-04 02:09:55 -0500

    Add Price Change Oscillator indicator

--- jarjarquant/indicator.py
+++ jarjarquant/indicator.py
@@ -670,3 +670,45 @@ class RegressionTrendDeviation(Indicator):
                 output[i] = 100 * norm.cdf(0.6 * error_contribution) - 50
 
         return output
+
+
+class PriceChangeOscillator(Indicator):
+
+    def __init__(self, ohlcv_df: pd.DataFrame, short_lookback: int = 5, long_lookback_multiplier: int = 5):
+        super().__init__(ohlcv_df)
+        self.short_lookback = short_lookback
+        self.long_lookback_multiplier = long_lookback_multiplier
+        self.indicator_type = 'continuous'
+
+    def calculate(self) -> np.ndarray:
+
+        close = self.df['Close'].values
+        prices = np.log(close)
+        n = len(close)
+
+        output = np.full(n, 0.0)
+        long_lookback = self.long_lookback_multiplier * self.short_lookback
+
+        # Calculate ATR over the long lookback period
+        atr = self.data_analyst.atr(
+            long_lookback, self.df['Open'], self.df['High'], self.df['Low'], self.df['Close']).values
+
+        for i in range(long_lookback, n):
+            # Calculate the short-term and long-term mean
+            short_ma = np.mean(
+                prices[i-self.short_lookback+1:i] - prices[i-self.short_lookback:i-1])
+            long_ma = np.mean(
+                prices[i-long_lookback+1:i] - prices[i-long_lookback:i-1])
+
+            const = 0.36 + (1/self.short_lookback) + 0.7 * \
+                np.log(0.5*self.long_lookback_multiplier) / 1.609
+            denom = atr[i] * const
+            denom = np.maximum(denom, 1e-8)
+
+            raw = (short_ma - long_ma) / denom
+            output[i] = 100 * norm.cdf(4 * raw) - 50
+
+        # Replace nan and inf values with 0
+        output = np.where(np.isnan(output), 0, output)
+
+        return output

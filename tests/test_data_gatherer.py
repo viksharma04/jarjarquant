59072fa5195b06c4134d1b2cebcb28a55efee01f
viksharma04 <me@vik-sharma.com>
2025-06-23 22:53:07 -0400

    refactor: Improve code formatting and enhance readability in test_data_gatherer.py

--- tests/test_data_gatherer.py
+++ tests/test_data_gatherer.py
@@ -1,5 +1,6 @@
-import pytest
 import pandas as pd
+import pytest
+
 from jarjarquant.data_gatherer import DataGatherer
 
 
@@ -9,30 +10,38 @@ def data_gatherer():
 
 
 def test_generate_random_normal(data_gatherer):
-    series = data_gatherer.generate_random_normal()
-    assert isinstance(series, pd.Series)
-    assert len(series) == 252
-    assert series.name == 'price'
+    df = data_gatherer.generate_random_normal()
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 252
 
 
-@pytest.mark.skip(reason="Excluded from CI/CD pipeline due to YF connection requirement")
+@pytest.mark.skip(
+    reason="Excluded from CI/CD pipeline due to YF connection requirement"
+)
 def test_get_yf_ticker():
     series = DataGatherer.get_yf_ticker("AAPL")
     assert isinstance(series, pd.DataFrame)
     assert not series.empty
-    assert all(col in series.columns for col in [
-               'Open', 'High', 'Low', 'Close', 'Volume'])
+    assert all(
+        col in series.columns for col in ["Open", "High", "Low", "Close", "Volume"]
+    )
 
 
-@pytest.mark.skip(reason="Excluded from CI/CD pipeline due to TWS connection requirement")
+@pytest.mark.skip(
+    reason="Excluded from CI/CD pipeline due to TWS connection requirement"
+)
 @pytest.mark.asyncio
 async def test_get_tws_ticker(data_gatherer):
-    df = await data_gatherer.get_tws_ticker(ticker='AAPL', end_date='20031126 15:59:00 US/Eastern')
+    df = await data_gatherer.get_tws_ticker(
+        ticker="AAPL", end_date="20031126 15:59:00 US/Eastern"
+    )
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
 
 
-@pytest.mark.skip(reason="Excluded from CI/CD pipeline due to YF connection requirement")
+@pytest.mark.skip(
+    reason="Excluded from CI/CD pipeline due to YF connection requirement"
+)
 def test_get_random_price_samples_yf(data_gatherer):
     dataframes = data_gatherer.get_random_price_samples_yf()
     assert isinstance(dataframes, list)
@@ -42,10 +51,14 @@ def test_get_random_price_samples_yf(data_gatherer):
         assert not df.empty
 
 
-@pytest.mark.skip(reason="Excluded from CI/CD pipeline due to TWS connection requirement")
+@pytest.mark.skip(
+    reason="Excluded from CI/CD pipeline due to TWS connection requirement"
+)
 @pytest.mark.asyncio
 async def test_get_random_price_samples_tws(data_gatherer):
-    dataframes = await data_gatherer.get_random_price_samples_tws(num_tickers_to_sample=1, years_in_sample=1)
+    dataframes = await data_gatherer.get_random_price_samples_tws(
+        num_tickers_to_sample=1, years_in_sample=1
+    )
     assert isinstance(dataframes, list)
     assert len(dataframes) == 1
     for df in dataframes:

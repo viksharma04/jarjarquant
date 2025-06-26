f8fd9bc72ca0432c8c86d9cf0e46558ce5b62cdb
viksharma04 <me@vik-sharma.com>
2025-06-25 21:03:44 -0400

    refactor: Update eval_result to be an instance variable in Indicator class

--- jarjarquant/indicators/base.py
+++ jarjarquant/indicators/base.py
@@ -59,7 +59,7 @@ class Indicator:
             n_bins_to_discretize (int, optional): Number of bins to use if indicator
             is continuous. Used for mutual information calculation. Defaults to 10.
         """
-        eval_result = IndicatorEvalResult()
+        self.eval_result = IndicatorEvalResult()
         values = self.calculate()
         if transform is not None:
             values = self.feature_engineer.transform(values, transform, **kwargs)
@@ -67,20 +67,22 @@ class Indicator:
                 values = np.asarray(values)
 
         self.data_analyst.visual_stationary_test(values)
-        eval_result.adf_test = self.data_analyst.adf_test(values, verbose)
-        eval_result.jb_normality_test = self.data_analyst.jb_normality_test(
+        self.eval_result.adf_test = self.data_analyst.adf_test(values, verbose)
+        self.eval_result.jb_normality_test = self.data_analyst.jb_normality_test(
             values, verbose
         )
-        eval_result.relative_entropy = self.data_analyst.relative_entropy(
+        self.eval_result.relative_entropy = self.data_analyst.relative_entropy(
             values, verbose
         )
-        eval_result.range_iqr_ratio = self.data_analyst.range_iqr_ratio(values, verbose)
+        self.eval_result.range_iqr_ratio = self.data_analyst.range_iqr_ratio(
+            values, verbose
+        )
 
         if self.indicator_type == "continuous":
             n_bins_to_discretize = (
                 n_bins_to_discretize if n_bins_to_discretize is not None else 10
             )
-            eval_result.mutual_information = self.data_analyst.mutual_information(
+            self.eval_result.mutual_information = self.data_analyst.mutual_information(
                 array=values,
                 lag=10,
                 n_bins=n_bins_to_discretize,
@@ -88,7 +90,7 @@ class Indicator:
                 verbose=verbose,
             )
         else:
-            eval_result.mutual_information = self.data_analyst.mutual_information(
+            self.eval_result.mutual_information = self.data_analyst.mutual_information(
                 array=values,
                 lag=10,
                 n_bins=None,
@@ -97,6 +99,6 @@ class Indicator:
             )
 
         for i in range(1, 11):
-            print(f"NMI @ lag {i} = {eval_result.mutual_information[i - 1]}")
+            print(f"NMI @ lag {i} = {self.eval_result.mutual_information[i - 1]}")
 
     # TODO: Implement a robust indicator evaluation report method which creates multiple instances of the indicator and averages the results

6c90970e36d5e28c905b955c0b64f2d072baa76a
viksharma04 <me@vik-sharma.com>
2025-06-25 21:05:20 -0400

    refactor: Improve code formatting and readability in labeller.py

--- jarjarquant/labeller.py
+++ jarjarquant/labeller.py
@@ -1,13 +1,16 @@
 """The labeller specializes in transforming raw price data into labels for ML using various methods"""
+
 # Imports
 from typing import Optional
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
 from .data_analyst import DataAnalyst
 
 
-class Labeller():
+class Labeller:
     "Class to label data - implements commonn methods used during labelling for financial ml"
 
     def __init__(self, ohlcv_df: pd.DataFrame):
@@ -18,7 +21,9 @@ class Labeller():
         """
         self._df = ohlcv_df
 
-    def inverse_cumsum_filter(self, series: pd.Series = None, h: float = 0.01, n: int = 2) -> pd.Series:
+    def inverse_cumsum_filter(
+        self, series: pd.Series = None, h: float = 0.01, n: int = 2
+    ) -> pd.Series:
         """
         Apply a cumulative sum filter to a time series based on a rolling period.
 
@@ -31,7 +36,7 @@ class Labeller():
         - pd.Series, boolean series where True indicates dates flagged by the filter
         """
         if series is None:
-            series = self._df['Close']
+            series = self._df["Close"]
 
         returns = series.pct_change()
         # Ensure the series is sorted by index (time)
@@ -41,11 +46,18 @@ class Labeller():
         rolling_cumsum = returns.rolling(window=n).apply(np.prod) - 1
 
         # Flag dates where the cumulative return is less than the absolute value of h
-        flagged = (rolling_cumsum.abs() < h)
+        flagged = rolling_cumsum.abs() < h
 
         return flagged
 
-    def event_sampling(self, method: str = 'vol_contraction', long_lookback: int = 100, short_lookback: int = 10, threshold: float = 2, price: Optional[str] = 'Close'):
+    def event_sampling(
+        self,
+        method: str = "vol_contraction",
+        long_lookback: int = 100,
+        short_lookback: int = 10,
+        threshold: float = 2,
+        price: Optional[str] = "Close",
+    ):
         """
         method can be 'vol_contraction', 'vol_expansion'
         contraction and expansion are defined based on three parameters: long lookback, short lookback, and threshold
@@ -56,19 +68,20 @@ class Labeller():
         long_vol = data_analyst.get_daily_vol(price_series, long_lookback)
         short_vol = data_analyst.get_daily_vol(price_series, short_lookback)
 
-        if method == 'vol_contraction':
-            ratio = long_vol/short_vol
-        elif method == 'vol_expansion':
-            ratio = short_vol/long_vol
+        if method == "vol_contraction":
+            ratio = long_vol / short_vol
+        elif method == "vol_expansion":
+            ratio = short_vol / long_vol
         else:
             raise ValueError(
-                "Method must be 'volatility_contraction' or 'volatility_expansion'")
+                "Method must be 'volatility_contraction' or 'volatility_expansion'"
+            )
 
         # Flag dates where the ratio is greater than the threshold
-        flagged = (ratio > threshold)
+        flagged = ratio > threshold
 
         # Add flagged column as 'event_flag' column to self._df
-        self._df['event_flag'] = flagged
+        self._df["event_flag"] = flagged
 
     @staticmethod
     def plot_with_flags(series: pd.Series, flagged: pd.Series):
@@ -84,18 +97,24 @@ class Labeller():
 
         # Plot the time series
         plt.figure(figsize=(10, 6))
-        plt.plot(series.index, series.values,
-                 label='Time Series', color='blue')
+        plt.plot(series.index, series.values, label="Time Series", color="blue")
 
         # Highlight flagged dates as red dots
-        plt.scatter(series.index[flagged], series.values[flagged],
-                    color='red', label='Flagged Dates')
+        plt.scatter(
+            series.index[flagged],
+            series.values[flagged],
+            color="red",
+            label="Flagged Dates",
+        )
 
         # Add labels and legend
-        plt.title(f"Time Series with Flagged Dates; Percent labels = {
-                  np.average(flagged)*100}%")
-        plt.xlabel('Date')
-        plt.ylabel('Return')
+        plt.title(
+            f"Time Series with Flagged Dates; Percent labels = {
+                np.average(flagged) * 100
+            }%"
+        )
+        plt.xlabel("Date")
+        plt.ylabel("Return")
         plt.legend()
 
         # Display the plot
@@ -115,24 +134,33 @@ class Labeller():
         Returns:
             pd.Series: series of datetime values
         """
-        t1 = Close.index.searchsorted(t_events+pd.Timedelta(days=num_days))
+        t1 = Close.index.searchsorted(t_events + pd.Timedelta(days=num_days))
         t1 = t1[t1 < Close.shape[0]]
-        t1 = (pd.Series(Close.index[t1], index=t_events[:t1.shape[0]]))
+        t1 = pd.Series(Close.index[t1], index=t_events[: t1.shape[0]])
         t1.index = t1.index.tz_localize(None)
         return t1
 
     @staticmethod
     def find_min_column(row):
-
-        if pd.isnull(row['pt']) & pd.isnull(row['sl']):
-            min_value = row[['pt', 'sl']].min()
+        if pd.isnull(row["pt"]) & pd.isnull(row["sl"]):
+            min_value = row[["pt", "sl"]].min()
         else:
             min_value = pd.Timestamp(0)
-        return row[['pt', 'sl']].idxmin() if min_value <= row['vb'] else row[['pt', 'sl', 'vb']].idxmin()
+        return (
+            row[["pt", "sl"]].idxmin()
+            if min_value <= row["vb"]
+            else row[["pt", "sl", "vb"]].idxmin()
+        )
 
     @staticmethod
-    def triple_barrier_method(Close: pd.Series, t_events: pd.DatetimeIndex = None, scale_pt_sl: bool = True, span: int = 100, pt_sl: int = 1.5, n_days: int = 10):
-
+    def triple_barrier_method(
+        Close: pd.Series,
+        t_events: pd.DatetimeIndex = None,
+        scale_pt_sl: bool = True,
+        span: int = 100,
+        pt_sl: int = 1.5,
+        n_days: int = 10,
+    ):
         Close.index = Close.index.tz_localize(None)
 
         # If scale pt_sl is True pt_sl is multiplied by the average period volatility over the scale_lookback period
@@ -159,64 +187,70 @@ class Labeller():
         v_bars = Labeller.get_vertical_barrier(t_events, Close, n_days)
         pt_sl = [pt_sl, -pt_sl]
 
-        events = pd.concat({'vb': v_bars, 'trgt': trgt},
-                           axis=1).dropna(subset=['trgt'])
+        events = pd.concat({"vb": v_bars, "trgt": trgt}, axis=1).dropna(subset=["trgt"])
 
-        exits = events[['vb']].copy(deep=True)
-        exits['sl'] = pd.NaT
-        exits['pt'] = pd.NaT
+        exits = events[["vb"]].copy(deep=True)
+        exits["sl"] = pd.NaT
+        exits["pt"] = pd.NaT
 
-        pt = pt_sl[0]*events['trgt']
-        sl = pt_sl[1]*events['trgt']
+        pt = pt_sl[0] * events["trgt"]
+        sl = pt_sl[1] * events["trgt"]
 
-        for event, vb in events['vb'].fillna(Close.index[-1]).items():
-
+        for event, vb in events["vb"].fillna(Close.index[-1]).items():
             price_path = Close[event:vb]
-            return_path = (price_path/Close[event]-1)
-            exits.loc[event, 'sl'] = return_path[return_path <
-                                                 # earliest stop loss
-                                                 sl[event]].index.min()
-            exits.loc[event, 'pt'] = return_path[return_path >
-                                                 # earliest profit taking
-                                                 pt[event]].index.min()
+            return_path = price_path / Close[event] - 1
+            exits.loc[event, "sl"] = return_path[
+                return_path
+                <
+                # earliest stop loss
+                sl[event]
+            ].index.min()
+            exits.loc[event, "pt"] = return_path[
+                return_path
+                >
+                # earliest profit taking
+                pt[event]
+            ].index.min()
 
-        exits['vb'] = exits['vb'].fillna(Close.index[-1])
-        exits['barrier_hit'] = exits.apply(
-            Labeller.find_min_column, axis=1)
-        exits['hit_date'] = exits[['vb', 'sl', 'pt']].min(axis=1)
-        exits['returns'] = Close[exits['hit_date']
-                                 ].values/Close[t_events].values - 1
-        exits['bin'] = np.sign(exits['returns'])
-        exits.loc[exits['barrier_hit'] == 'vb', 'bin'] = 0
+        exits["vb"] = exits["vb"].fillna(Close.index[-1])
+        exits["barrier_hit"] = exits.apply(Labeller.find_min_column, axis=1)
+        exits["hit_date"] = exits[["vb", "sl", "pt"]].min(axis=1)
+        exits["returns"] = Close[exits["hit_date"]].values / Close[t_events].values - 1
+        exits["bin"] = np.sign(exits["returns"])
+        exits.loc[exits["barrier_hit"] == "vb", "bin"] = 0
 
-        return exits[['hit_date', 'bin', 'returns']]
+        return exits[["hit_date", "bin", "returns"]]
 
-    def add_labels(self, method: Optional[str] = 'triple_barrier', price: Optional[str] = 'Close', **kwargs):
-
+    def add_labels(
+        self,
+        method: Optional[str] = "triple_barrier",
+        price: Optional[str] = "Close",
+        **kwargs,
+    ):
         labels = None
 
         # If hit_date, bin, and returns column are present in the dataframe, remove them
-        if 'hit_date' in self._df.columns:
-            self._df.drop(columns=['hit_date', 'bin', 'returns'], inplace=True)
+        if "hit_date" in self._df.columns:
+            self._df.drop(columns=["hit_date", "bin", "returns"], inplace=True)
 
-        if method == 'triple_barrier':
-            if 'event_flag' in self._df.columns:
-                t_events = self._df['event_flag'].dropna().index
+        if method == "triple_barrier":
+            if "event_flag" in self._df.columns:
+                t_events = self._df["event_flag"].dropna().index
                 labels = self.triple_barrier_method(
-                    self._df[price], t_events=t_events, **kwargs)
+                    self._df[price], t_events=t_events, **kwargs
+                )
             else:
-                labels = self.triple_barrier_method(
-                    self._df[price], **kwargs)
+                labels = self.triple_barrier_method(self._df[price], **kwargs)
 
-        self._df = self._df.join(labels, how='left')
+        self._df = self._df.join(labels, how="left")
 
     @staticmethod
     def num_co_events(close_idx, t_exits):
-        '''
+        """
         Compute the number of concurrent events per bar across the entire `closeIdx` range.
 
         Any event that starts before the maximum of `t1` impacts the count.
-        '''
+        """
         # 1) Handle unclosed events (events with NaN end date)
         t_exits.fillna(close_idx[-1])
         # unclosed events affect the count
@@ -225,12 +259,11 @@ class Labeller():
         # events that end after the first closeIdx time
         t_exits = t_exits[t_exits >= close_idx[0]]
         # events that start at or before the latest event in t1
-        t_exits = t_exits.loc[:t_exits.max()]
+        t_exits = t_exits.loc[: t_exits.max()]
 
         # 3) Initialize a count series covering the entire closeIdx range
-        iloc = close_idx.searchsorted(
-            np.array([t_exits.index[0], t_exits.max()]))
-        count = pd.Series(0, index=close_idx[iloc[0]:iloc[1] + 1])
+        iloc = close_idx.searchsorted(np.array([t_exits.index[0], t_exits.max()]))
+        count = pd.Series(0, index=close_idx[iloc[0] : iloc[1] + 1])
 
         # 4) Count events that span each bar in closeIdx
         for t_in, t_out in t_exits.items():
@@ -242,7 +275,7 @@ class Labeller():
     def average_uniqueness(t_exits, co_events):
         wght = pd.Series(index=t_exits.index)
         for t_in, t_out in t_exits.items():
-            wght.loc[t_in] = (1./co_events.loc[t_in:t_out]).mean()
+            wght.loc[t_in] = (1.0 / co_events.loc[t_in:t_out]).mean()
 
         return wght
 
@@ -263,23 +296,23 @@ class Labeller():
         ret = np.log(Close).diff()  # log-returns, so that they are additive
         wght = pd.Series(index=t_exits.index)
         for t_in, t_out in t_exits.loc[wght.index].items():
-            wght.loc[t_in] = (ret.loc[t_in:t_out] /
-                              co_events.loc[t_in:t_out]).sum()
+            wght.loc[t_in] = (ret.loc[t_in:t_out] / co_events.loc[t_in:t_out]).sum()
         return wght.abs()
 
-    def add_sample_weights(self, price: Optional[str] = 'Close'):
-
+    def add_sample_weights(self, price: Optional[str] = "Close"):
         sw = None
 
-        if 'hit_date' not in self._df.columns:
+        if "hit_date" not in self._df.columns:
             raise ValueError(
-                "hit_date column not found in the dataframe. Please add labels first.")
+                "hit_date column not found in the dataframe. Please add labels first."
+            )
 
         # If 'sample_weight' column is present in the dataframe, remove it
-        if 'sample_weight' in self._df.columns:
-            self._df.drop(columns=['sample_weight'], inplace=True)
+        if "sample_weight" in self._df.columns:
+            self._df.drop(columns=["sample_weight"], inplace=True)
 
         sw = self.get_sample_weights(
-            Close=self._df[price], t_exits=self._df['hit_date'])
+            Close=self._df[price], t_exits=self._df["hit_date"]
+        )
 
-        self._df['sample_weight'] = sw
+        self._df["sample_weight"] = sw

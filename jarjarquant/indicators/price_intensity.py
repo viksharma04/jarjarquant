fix: handle NaN values in Spearman correlation fix: adjust returns calculation in threshold optimization study to remove data snooping fix: handle division by zero in price intensity calculation

jarjarquant/indicators/price_intensity.py
@@ -1,10 +1,10 @@
 import numpy as np
-import polars as pl
 import pandas as pd
+import polars as pl
 from scipy.stats import norm
 
 from jarjarquant.indicators.base import Indicator
-from jarjarquant.indicators.registry import register_indicator, IndicatorType
+from jarjarquant.indicators.registry import IndicatorType, register_indicator
 
 
 @register_indicator(IndicatorType.PRICE_INTENSITY)
@@ -27,14 +27,15 @@ class PriceIntensity(Indicator):
         output = np.full(n, 0.0)
 
         # Special case for the first value
-        output[0] = (close[0] - _open[0]) / (high[0] - low[0])
+        range_0 = high[0] - low[0]
+        output[0] = (close[0] - _open[0]) / range_0 if range_0 != 0 else 0.0
 
         # Calculate Raw Price Intensity
         for i in range(1, n):
             denom = np.maximum.reduce(
                 [high[i] - low[i], high[i] - close[i - 1], close[i - 1] - low[i]]
             )
-            output[i] = (close[i] - _open[i]) / denom
+            output[i] = (close[i] - _open[i]) / denom if denom != 0 else 0.0
 
         # Smooth the Price Intensity values
         output = (